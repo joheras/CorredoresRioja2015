c983b482531e698907ac8ecb205a411a25bbc5e1
joheras <joheras@gmail.com>
2018-09-12 09:18:36 +0200

    In Memory

--- src/App/CorredoresRiojaDomain/Repository/Infrastructure/InMemoryCorredorRepository.php
+++ src/App/CorredoresRiojaDomain/Repository/Infrastructure/InMemoryCorredorRepository.php
@@ -10,7 +10,7 @@ class InMemoryCorredorRepository implements ICorredorRepository {
     private $corredores;
 
     function __construct() {
-        $this->corredores[] = new Corredor(16613711, 'Jonathan', 'Heras Vicente', 'joheras@gmail.com', '$2a$12$pgOouPm3E8mcAYT9WraLluceSUmbJFuPogELZZHPM36eBYX.iR20q', 'Calle Falsa', new \DateTime("1985-08-15"));
+        $this->corredores[] = new Corredor(00000000, 'Jonathan', 'Heras Vicente', 'joheras@gmail.com', '$2a$12$pgOouPm3E8mcAYT9WraLluceSUmbJFuPogELZZHPM36eBYX.iR20q', 'Calle Falsa', new \DateTime("1985-08-15"));
         $this->corredores[] = new Corredor(11111111, 'Eloy', 'Mata', 'eloy.mata@unirioja.es', '$2a$12$pgOouPm3E8mcAYT9WraLluceSUmbJFuPogELZZHPM36eBYX.iR20q', 'Ed Vives', new \DateTime("1970-06-07"));
         $this->corredores[] = new Corredor(22222222, 'Pepe', 'Perez', 'pepe.perez@gmail.com', '$2a$12$pgOouPm3E8mcAYT9WraLluceSUmbJFuPogELZZHPM36eBYX.iR20q', 'Luis de Ulloa', new \DateTime("1980-02-23"));
     }
